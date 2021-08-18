5445509fb2579c291f1512dc57b5f71731779f5a
Taylor Jones <taylor@taylorwjones.com>
2021-08-18 16:41:12 -0500

    Created the test for making sure a marker is placed in the correct spot and the logic behind that method

--- lib/gameboard.rb
+++ lib/gameboard.rb
@@ -2,14 +2,24 @@
 
 # Gameboard for the Tic Tac Toe game
 class Gameboard
-  attr_accessor :game_board
+  attr_accessor :board
 
   def initialize
-    @game_board = []
-    i = 0
-    while i < 3
-      game_board.push(['', '', ''])
-      i += 1
+    @board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', '']
+    ]
+  end
+
+  def place_marker(row, spot, mark)
+    case row
+    when 'A'
+      board[0][spot - 1] = mark
+    when 'B'
+      board[1][spot - 1] = mark
+    when 'C'
+      board[2][spot - 1] = mark
     end
   end
 end
--- spec/gameboard_spec.rb
+++ spec/gameboard_spec.rb
@@ -1,12 +1,21 @@
 # spec/gameboard_spec.rb
 
-require_relative '../lib/gameboard.rb'
+require_relative '../lib/gameboard'
 
 describe Gameboard do
-	describe "Initialize the gameboard" do
-		it "returns an array filled with 3 seperate arrays all filled with blank '' spots" do
-			gameboard = Gameboard.new
-			expect(gameboard.game_board).to eql([['','',''],['','',''],['','','']])
-		end
-	end
+  describe 'Initialize the gameboard' do
+    it "returns an array filled with 3 seperate arrays all filled with blank '' spots" do
+      gameboard = Gameboard.new
+      expect(gameboard.board).to eql([['', '', ''], ['', '', ''], ['', '', '']])
+    end
+  end
+  describe
+
+  describe 'Placing a players marker on the board with given coordinates' do
+    it 'returns the gameboard with the correct spot marked' do
+      gameboard = Gameboard.new
+      gameboard.place_marker('B', 3, 'X')
+      expect(gameboard.board).to eql([['', '', ''], ['', '', 'X'], ['', '', '']])
+    end
+  end
 end
