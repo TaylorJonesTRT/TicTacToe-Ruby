888ba69d7ec185c9ea5fe522c6b84dabff306793
Taylor Jones <taylor@taylorwjones.com>
2021-08-21 21:38:18 -0500

    Created the game.rb file

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
 # player.rb
-
 class Player
-	attr_accessor :name, :symbol
-	def initialize(name, symbol)
-		@name = name
-		@symbol = symbol
-	end
+  attr_accessor :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
 end
