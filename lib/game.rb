8b67fc2d214f028f401b520e1e4c64c05bc59989
Taylor Jones <taylor@taylorwjones.com>
2021-08-23 20:16:15 -0500

    Starting on the game_start_screen method in the Game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,14 @@
 class Game
 	def initialize
 		@board = Board.new
+		@amount_of_players
 		@first_player = nil
 		@second_player = nil
 		@current_player = nil
 	end
+
+	def game_start_screen
+		puts "Welcome to the game of Tic Tac Toe!"
+		puts "How many players will be playing? (1 or 2)"
+		@amount_of_players = gets
 end
