44b3d1eee245c3d43d53510d4b6c88600890ed80
Taylor Jones <taylor@taylorwjones.com>
2021-08-31 21:33:41 -0500

    ✨ New methods added to the Game class to start on.

--- lib/game.rb
+++ lib/game.rb
@@ -1,36 +1,50 @@
 # lib/game.rb
 class Game
-	def initialize
-		@board = Board.new
-		@amount_of_players
-		@first_player = nil
-		@second_player = nil
-		@current_player = nil
-	end
+  def initialize
+    @board = Board.new
+    @amount_of_players
+    @first_player = nil
+    @second_player = nil
+    @current_player = nil
+  end
 
-	def game_start_screen
-		puts "Welcome to the game of Tic Tac Toe!"
-		puts "How many players will be playing? (1 or 2)"
-		@amount_of_players = gets.chomp
-		case amount_of_players
-		when 2
-			puts "What is the first players name"
-			first_player_name = gets.chomp
-			puts "What is the first players symbol? (X or O)"
-			first_player_symbol = gets.chomp
-			puts "What is the second players name?"
-			second_players_name = gets.chomp
-			case first_player_symbol
-			when X
-				second_player_symbol = "O"
-			else
-				second_player_symbol = "X"
-			end
-			@first_player = Player.new(first_player_name, first_player_symbol, is_computer = false)
-			@second_player = Player.new(second_players_name, second_player_symbol, is_computer = false)
-		else
-			puts "What is your name?"
-			first_player_name = gets.chomp
-			@first_player = Player.new(first_player_name, 'X', is_computer = false)
-			@second_player = Player.new('Computer', 'O', is_computer = true)
+  # def play_game
+  # end
+
+  def game_start_screen
+    puts "Welcome to the game of Tic Tac Toe!"
+    puts "How many players will be playing? (1 or 2)"
+    @amount_of_players = gets.chomp
+    case amount_of_players
+    when 2
+      puts "What is the first players name"
+      first_player_name = gets.chomp
+      puts "What is the first players symbol? (X or O)"
+      first_player_symbol = gets.chomp
+      puts "What is the second players name?"
+      second_players_name = gets.chomp
+      case first_player_symbol
+      when X
+        second_player_symbol = "O"
+      else
+        second_player_symbol = "X"
+      end
+      @first_player = Player.new(first_player_name, first_player_symbol, is_computer = false)
+      @second_player = Player.new(second_players_name, second_player_symbol, is_computer = false)
+    else
+      puts "What is your name?"
+      first_player_name = gets.chomp
+      @first_player = Player.new(first_player_name, 'X', is_computer = false)
+      @second_player = Player.new('Computer', 'O', is_computer = true)
+    end
+  end
+
+  # def player_move(player)
+  # end
+
+  # def switch_current_player
+  # end
+
+  # def game_over
+  # end
 end
