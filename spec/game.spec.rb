15b462d62e0918019ae16650bb39decbd4859591
Taylor Jones <taylor@taylorwjones.com>
2021-09-04 15:32:51 -0500

    Setting up test for switching players

---
+++ spec/game.spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require '../lib/gameboard'
+require '../lib/game'
+require '../lib/player'
+
+describe Game do
+  describe 'Game switches players' do
+    it 'Takes a player and returns the other player' do
+    end
+  end
+end
