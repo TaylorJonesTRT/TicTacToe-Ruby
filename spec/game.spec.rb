60b11c91bd000f84e4e13c59aa9bea0f871fb5f2
Taylor Jones <taylor@taylorwjones.com>
2021-09-04 15:39:25 -0500

    Created test for switching player

--- spec/game.spec.rb
+++ spec/game.spec.rb
@@ -4,9 +4,23 @@ require '../lib/gameboard'
 require '../lib/game'
 require '../lib/player'
 
+# Mock function for changing the player
+def switch_player(player)
+  current_player = case player.name
+  when 'Player 1'
+    second_player
+  else
+    first_player
+  end
+  current_player
+end
+
 describe Game do
   describe 'Game switches players' do
     it 'Takes a player and returns the other player' do
+      first_player = Player.new('Player 1', 'X')
+      second_player = Player.new('Computer', 'O')
+      expect(switch_player(first_player).to be(second_player))
     end
   end
 end
